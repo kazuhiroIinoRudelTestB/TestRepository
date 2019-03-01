c3edecf6e4513831bb361085c9f5d86c7a44fd48
kazuhiro iino <kazuhiro_iino@rudel.jp>
2019-03-01 16:26:58 +0900

    Changes to be committed: 	new file:   RudelTest1.html 	new file:   outputPage.php

---
+++ outputPage.php
@@ -0,0 +1,15 @@
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kazuhiro_iino
+ * Date: 2019/03/01
+ * Time: 15:38
+ */
+$outputText = $_POST['inputText'];
+
+print("あなたが入力した文字は".$outputText."です。");
+
+?>
+
+https://github.com/kazuhiroIinoRudelTestB/TestRepository.git
